83b1711a3c61b43aac1c32e2cbec89f538aadae6
TiagoCarneiroDoNascimento <107155765+TiagoGeografiaBacharelado@users.noreply.github.com>
2024-11-22 10:33:58 -0300

    Update retro_game_database.sql
    Adição da Tabela Desenvolvedor.

--- retro_game_database.sql
+++ retro_game_database.sql
@@ -1,3 +1,7 @@
 CREATE DATABASE retro_game_database;
 USE retro_game_database;
 
+CREATE TABLE Desenvolvedor (
+    id_desenvolvedor INT PRIMARY KEY AUTO_INCREMENT, 
+    nome VARCHAR(100) NOT NULL                       
+);
